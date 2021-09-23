a7da4f252e0e86b1b90deadf30ec2356fc69e77f
Axalon Vargas <axel72avs@outlook.com>
2021-09-23 08:39:35 -0600

    New features and need to fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,20 @@ import { HttpClient } from "@angular/common/http";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'exchangerateweb';
+  //Obtener la fecha actual
   datenow = Date.now();
+  //Array donde contiene toda la informacion
   dataExchange: [] = [];
+  //Para segun el estado ocultar o visualizar una calculadora u otra
+  hideBuySell = true;
+  //Variables para la calculadora compra
+  BuyCalc1 = 0;
+  BuyCalc2 = 0;
+  //Variables para la calculadora venta
+  SellCalc1 = 0;
+  SellCalc2 = 0;
 
   constructor (private http: HttpClient) {}
 
@@ -22,5 +32,52 @@ export class AppComponent {
       console.log(data);
       this.dataExchange = JSON.parse(JSON.stringify(data));
     }); 
+  }
+
+  changeBuyCalc1(e: any, SpanValue: any) {
+    if (e.target.value != 0) {
+      this.BuyCalc2 = SpanValue * e.target.value  
+    } else {
+      e.target.value = ''
+      this.BuyCalc1 = e.target.value;
+      this.BuyCalc2 = e.target.value;
+    }    
+  }
+
+  changeBuyCalc2(e: any, SpanValue: any) {
+    if (e.target.value != 0) {
+      this.BuyCalc1 = e.target.value / SpanValue;
+    } else {
+      e.target.value = ''
+      this.BuyCalc1 = e.target.value;
+      this.BuyCalc2 = e.target.value;
+    }
+  }
+
+  changeSellCalc1(e: any, SpanValue: any) {
+    if (e.target.value != 0) {
+      this.SellCalc2 = e.target.value / SpanValue;
+    } else {
+      e.target.value = ''
+      this.SellCalc1 = e.target.value;
+      this.SellCalc2 = e.target.value;
+    }
+  }
+
+  changeSellCalc2(e: any, SpanValue: any) {
+    if (e.target.value != 0) {
+      this.SellCalc1 = SpanValue * e.target.value  
+    } else {
+      e.target.value = ''
+      this.SellCalc1 = e.target.value;
+      this.SellCalc2 = e.target.value;
+    }    
+  }
+  
+  resetAll() {
+    this.BuyCalc1 = 0;
+    this.BuyCalc2 = 0;
+    this.SellCalc1 = 0;
+    this.SellCalc2 = 0;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
